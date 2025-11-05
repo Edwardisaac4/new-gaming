b119396bad8769a4dee8733c505342fa1bede92d
Edwardisaac4 <isaacdrumz4@gmail.com>
2025-11-05 13:37:42 +0100

    new files

---
+++ gaming-project/src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="vw-100 bg-black flex justify-center">
+      <h1 className="text-5xl font-bold text-cyan-400">Hello Tailwind!</h1>
+    </div>
+  );
+};
+
+export default App;
